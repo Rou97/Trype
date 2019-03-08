f10a8e3b67eca7cb3b8029e76c9746b1d40afc5d
Raul Delgado <raul@MacBook-Pro-de-Raul.local>
2019-03-08 12:55:40 +0100

    book routes and add a book

--- routes/splash.js
+++ routes/splash.js
@@ -1,6 +1,7 @@
 // Raúl Miguel acuerdate de poner los putos flash
 const express = require('express');
 const User = require('../models/User');
+// const api = require('google-books-search');
 
 const { requireUser } = require('../middlewares/auth');
 
@@ -20,8 +21,23 @@ router.get('/profile', requireUser, async (req, res, next) => {
   }
 });
 
-router.get('/books', (req, res, next) => {
-  res.render('main/books');
+router.get('/books', async (req, res, next) => {
+  const { _id } = req.session.currentUser;
+  try {
+    const user = await User.findById(_id);
+    res.render('main/books', { user });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/books/add-book', (req, res, next) => {
+  res.render('main/add-book');
+});
+
+router.post('/books/add-book/search', (req, res, next) => {
+  const { title } = req.body;
+  res.render('main/add-book', { title });
 });
 
 router.get('/matches', (req, res, next) => {
